4ecd37c75aeb8ed952c61140225ab7e3c984d357
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-08 02:29:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,10 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
-def using_sort(array)
-
+def using_sort(array, element, string)
+  arr = ["hi","hello","bye"]
+  arr.sort { |a,b| b <=> a}
 end
 
 def using_reverse(array)
